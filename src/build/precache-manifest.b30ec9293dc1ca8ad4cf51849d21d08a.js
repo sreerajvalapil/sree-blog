5a5f9e88d5e231e9fc7ba267dd60c67e5854527a
sreeraj <sree.valapil@gmail.com>
2020-05-11 23:58:37 +0200

    mongo backend app

---
+++ src/build/precache-manifest.b30ec9293dc1ca8ad4cf51849d21d08a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088d681967983a8937c22c67de32409a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d78d2663c66537b29b20",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "779ab05483c50a4f747b",
+    "url": "/static/js/2.6b354c1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b354c1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78d2663c66537b29b20",
+    "url": "/static/js/main.be65e143.chunk.js"
+  },
+  {
+    "revision": "97828cdace4138e8ec14",
+    "url": "/static/js/runtime-main.07c2870d.js"
+  }
+]);
